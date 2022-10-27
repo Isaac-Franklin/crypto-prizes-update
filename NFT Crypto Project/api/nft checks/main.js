c57ec941d1e0f731f8f8f8bdda61781afd336eae
Isaac Franklin T <98078616+Isaac-Franklin@users.noreply.github.com>
2022-10-27 01:16:05 +0100

    Delete NFT Crypto Project directory

--- NFT Crypto Project/api/nft checks/main.js
+++
@@ -1,8 +0,0 @@
-let access = 'https://eth-mainnet.g.alchemy.com/nft/v2/{A3Rd7TR2_kQf9knuYGlOtZhfBdiYhB_i}/getNFTs'
-
-const action = () => {
-    fetch(access)
-        .then((res) => res.json())
-        .then((res) => console.log(res))
-}
-action()
